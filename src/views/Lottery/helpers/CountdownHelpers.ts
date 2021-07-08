49a91a7bf5dade4ab63e57c0f544cd9d0e37bc56
shrijana <shrijana1991@gmail.com>
2021-07-08 23:16:35 +0545

    update draw time

--- src/views/Lottery/helpers/CountdownHelpers.ts
+++ src/views/Lottery/helpers/CountdownHelpers.ts
@@ -12,7 +12,7 @@ const getSeconds = () => {
   const offset = -300; // Timezone offset for EST in minutes.
   const nowDate = new Date(dt.getTime() + offset*60*1000)
   const dy = 6 // Sunday through Saturday, 0 to 6
-  const countertime = new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate(), 13, 0, 0) // 13 out of 24 hours = 1pm
+  const countertime = new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate(), 12, 0, 0) // 13 out of 24 hours = 1pm
 
   const curtime = nowDate.getTime() // current time
   const atime = countertime.getTime() // countdown time
