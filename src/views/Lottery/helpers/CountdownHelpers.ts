0f8a355c0808a014ce1f5d6d497c0d262a860b63
shrijana <shrijana1991@gmail.com>
2021-07-11 16:27:34 +0545

    update timer

--- src/views/Lottery/helpers/CountdownHelpers.ts
+++ src/views/Lottery/helpers/CountdownHelpers.ts
@@ -15,7 +15,7 @@ const getDrawSeconds = () => {
   dt.setTime(dt.getTime()+dt.getTimezoneOffset()*60*1000);
   const offset = -300; // Timezone offset for EST in minutes.
   const nowDate = new Date(dt.getTime() + offset*60*1000)
-  const dy = 13 // Sunday through Saturday, 0 to 6
+  const dy = 6 // Sunday through Saturday, 0 to 6
   const countertime = new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate(), 13, 0, 0) // 13 out of 24 hours = 1pm
 
   const curtime = nowDate.getTime() // current time
@@ -40,8 +40,8 @@ const getSalesSeconds = () => {
   dt.setTime(dt.getTime()+dt.getTimezoneOffset()*60*1000);
   const offset = -300; // Timezone offset for EST in minutes.
   const nowDate = new Date(dt.getTime() + offset*60*1000)
-  const dy = 13 // Sunday through Saturday, 0 to 6
-  const countertime = new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate(), 13, 0, 0) // 13 out of 24 hours = 1pm
+  const dy = 6 // Sunday through Saturday, 0 to 6
+  const countertime = new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate(), 12, 0, 0) // 13 out of 24 hours = 1pm
 
   const curtime = nowDate.getTime() // current time
   const atime = countertime.getTime() // countdown time
